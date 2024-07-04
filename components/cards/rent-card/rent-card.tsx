7bc81b3536ec5dc0b6965c29a5e89bfa911f6221
recor-glitch <rishisarmah4@gmail.com>
2024-07-04 18:58:20 +0530

    popular tag added

--- components/cards/rent-card/rent-card.tsx
+++ components/cards/rent-card/rent-card.tsx
@@ -6,85 +6,107 @@ import { dollar, rupee } from "@/constants";
 import BedIcon from "@/public/bed-icon.svg";
 import WashIcon from "@/public/wash-icon.svg";
 import DimensionIcon from "@/public/dimension-icon.svg";
+import StarIcon from "@/public/stars-icon.svg";
+import PointedEdge from "@/public/pointed-edge.svg";
 
 export interface rentProps {
   className?: string;
   rent: RentRoomType;
   isSmall?: boolean;
+  isLiked?: boolean;
+  showLike?: boolean;
 }
 
-function RentCard({ className, rent, isSmall }: rentProps) {
+function RentCard({
+  className,
+  rent,
+  isSmall,
+  isLiked = false,
+  showLike = false,
+}: rentProps) {
   return (
     <div
-      className={`rentContainer ${className} ${
-        isSmall && "h-mi-rent-card w-mi-rent-card"
-      }`}
+      className={`${className} ${isSmall && "h-mi-rent-card w-mi-rent-card"}`}
     >
-      <Image
-        src={rent.images.length != 0 ? rent.images[0].url : DummyImg}
-        alt={rent.images[0].alt}
-        className="flex object-cover rounded-t-default w-full"
-        width={200}
-        height={250}
-      />
-      <div
-        className={` flex flex-col ${
-          isSmall ? "gap-xxs p-2" : "gap-default py-btn-v px-btn-h"
-        }`}
-      >
-        <span className="w-full inline-flex items-center">
-          <p className="text-primary text-md-title font-extrabold">
-            {rent.price.currency === "INR" ? rupee : dollar}
-          </p>
-          <p className="text-primary text-md-title font-extrabold">
-            {" "}
-            {rent.price.amount}/{" "}
-          </p>
-          <p className="text-md-subtitle-secondary font-medium">
-            {rent.price.period.toLowerCase().slice(0, -2)}
-          </p>
-        </span>
-        <p className="text-text-primary text-md-title font-bold">
-          {rent.title}
-        </p>
-        <p className="text-text-primary text-md-subtitle-primary font-medium line-clamp-1 text-ellipsis overflow-hidden">
-          {rent.address}
-        </p>
-        <div className="divider-h h-[2px]" />
-        <div className="flex flex-row gap-sm">
-          {/* BED */}
-          <div className="flex flex-row justify-center items-center gap-xs">
-            <Image className="text-primary" src={BedIcon} alt="Bed icon" />
-            <p className="text-text-secondary-dark text-md-subtitle-secondary">
-              {rent.type === "SINGLE"
-                ? 1
-                : rent.type === "DOUBLE"
-                ? 2
-                : rent.beds}
+      {/* POPULAR */}
+      {rent.isPopular && (
+        <div className="flex justify-around items-center absolute min-w-[7.063rem] min-h-[2.5rem] bg-primary z-30 top-[11rem] left-0 transform translate-x-[-0.5rem] rounded-t-lg rounded-br-lg">
+          <Image src={StarIcon} alt="popular star icon" />
+          <p className="text-sm-subtitle text-white font-bold">POPULAR</p>
+        </div>
+      )}
+      {rent.isPopular && (
+        <Image
+          src={PointedEdge}
+          alt="pointer for popular tag"
+          height={8}
+          width={8}
+          className="absolute top-[13.5rem] transform translate-x-[-0.5rem]"
+        />
+        // <div className="bg-primary absolute rounded-bl-sm z-10 transform :bottom-0 left-0 translate-y-[1.2rem] translate-x-[-0.35rem] top-[11.9rem] rotate-45 h-3 w-3"></div>
+      )}
+      <div className="rentContainer">
+        <Image
+          src={rent.images.length != 0 ? rent.images[0].url : DummyImg}
+          alt={rent.images[0].alt}
+          className="flex object-cover rounded-t-default w-full"
+          width={200}
+          height={250}
+        />
+        <div
+          className={`flex flex-col ${
+            isSmall ? "gap-xxs p-2" : "gap-xs py-btn-v px-btn-h"
+          }`}
+        >
+          <span className="w-full inline-flex items-center text-primary text-md-title font-extrabold">
+            <p className="">{rent.price.currency === "INR" ? rupee : dollar}</p>
+            <p className=""> {rent.price.amount}/ </p>
+            <p className="text-md-subtitle-secondary font-medium">
+              {rent.price.period.toLowerCase().slice(0, -2)}
             </p>
-          </div>
-          {/* WASHROOM */}
-          <div className="flex flex-row justify-center items-center gap-xs">
-            <Image
-              className="text-primary"
-              src={WashIcon}
-              alt="Wash room icon"
-            />
-            <p className="text-text-secondary-dark text-md-subtitle-secondary">
-              {rent.washroom.count}
-            </p>
-          </div>
-          {/* DIMENSIONS */}
-          <div className="inline-flex flex-row justify-center items-center gap-xs">
-            <Image
-              className="text-primary"
-              src={DimensionIcon}
-              alt="Dimension icon"
-            />
-            <p className="text-text-secondary-dark text-md-subtitle-secondary line-clamp-1">
-              {rent.size.dimensions.length}x{rent.size.dimensions.width}{" "}
-              {rent.size.type === "FEET" ? "ft" : "m"}
-            </p>
+          </span>
+          <p className="text-text-primary text-md-title font-bold">
+            {rent.title}
+          </p>
+          <p className="text-text-primary text-md-subtitle-primary font-medium line-clamp-1 text-ellipsis overflow-hidden">
+            {rent.address}
+          </p>
+          <div className="divider-h h-[2px]" />
+          <div className="flex flex-row gap-sm">
+            {/* BED */}
+            <div className="flex flex-row justify-center items-center gap-xs">
+              <Image className="text-primary" src={BedIcon} alt="Bed icon" />
+              <p className="text-text-secondary-dark text-md-subtitle-secondary">
+                {rent.type === "SINGLE"
+                  ? 1
+                  : rent.type === "DOUBLE"
+                  ? 2
+                  : rent.beds}
+              </p>
+            </div>
+            {/* WASHROOM */}
+            <div className="flex flex-row justify-center items-center gap-xs">
+              <Image
+                className="text-primary"
+                src={WashIcon}
+                alt="Wash room icon"
+              />
+              <p className="text-text-secondary-dark text-md-subtitle-secondary">
+                {rent.washroom.count}
+              </p>
+            </div>
+            {/* DIMENSIONS */}
+            <div className="inline-flex flex-row justify-center items-center gap-xs">
+              <Image
+                className="text-primary"
+                src={DimensionIcon}
+                alt="Dimension icon"
+              />
+              <p className="text-text-secondary-dark text-md-subtitle-secondary line-clamp-1">
+                {rent.size.dimensions.length}x{rent.size.dimensions.width}{" "}
+                {rent.size.type === "FEET" ? "ft" : "m"}
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- constants.ts
+++ constants.ts
@@ -11,7 +11,7 @@ export const dummyRoomRent: RentRoomType[] = [
     type: "VILLA",
     address: "2821 Lake Sevilla, Palm Harbor, TX",
     desc: "Style country house is quiet owners take care of itself. Safe and clean with warm and personalized. Parking Facilities Situated near the bypass road east Getting comfortable Airport - Walking Street - downtown 10 minutes - 15 minutes Wat Rong Khun is ideal for those seeking a private residence, not a building.",
-    isPopular: false,
+    isPopular: true,
     likeCount: 0,
     price: { amount: 1500, currency: "INR", period: "MONTHLY" },
     balcony: 2,
